71af7d28b963c3488b60c3280c37ea28921e45cc
Mike <mikefelix@gmail.com>
2019-07-30 13:53:30 -0400

    Add basic components and tests

---
+++ src/Input.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, storeFactory } from '../test/testUtils';
+import Input from './Input';
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Input store={store}/>).dive().dive();
+  return wrapper;
+};
+
+setup();
+
+describe('render', () => {
+  describe('when word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    it('renders component without error', () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    it('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    it('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
+  });
+
+  describe('when word has been guessed', () => {
+    it('renders component without error', () => {
+
+    });
+    it('does not render input box', () => {
+
+    });
+    it('does not render submit button', () => {
+
+    });
+  });
+
+});
+
+describe('update state', () => {
+
+});
---
+++ test/testUtils.js
@@ -0,0 +1,26 @@
+import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
+
+/**
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes, 
+    conformingProps,
+    'prop',
+    component.name);
+  expect(propError).toBeUndefined();
+};
